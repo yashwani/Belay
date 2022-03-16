d1bbf9151a810aa5e17e658ef323a4b7b7030bb6
Yash Wani <yashwani@uchicago.edu>
2022-03-16 10:14:08 -0700

    can see replies

--- db/20220315T0412080000_create_tables.sql
+++ db/20220315T0412080000_create_tables.sql
@@ -9,7 +9,6 @@ INSERT INTO channels (channelname) VALUES ("BTC");
 INSERT INTO channels (channelname) VALUES ("fats domino");
 
 
-
 drop table if exists messages;
 create table if not exists messages (
   id INT AUTO_INCREMENT PRIMARY KEY,
@@ -23,6 +22,8 @@ INSERT INTO messages (channel_id, user, content) VALUES (0, "rufus", " bark bark
 INSERT INTO messages (channel_id, user, content) VALUES (1, "yash", "hi nala this is chat 2");
 INSERT INTO messages (channel_id, user, content) VALUES (1, "nala", "soft bark bark bark bark");
 
+
+
 drop table if exists auth;
 create table if not exists auth (
   authkey varchar(255) NOT NULL,
@@ -34,3 +35,27 @@ INSERT INTO auth (authkey, channel_id) VALUES ('testauthkey', 0);
 INSERT INTO auth (authkey, channel_id) VALUES ('testauthkey', 1);
 
 
+drop table if exists users;
+create table if not exists users(
+  username varchar(255) NOT NULL,
+  pwd varchar(255) NOT NULL,
+  authkey varchar(255) NOT NULL,
+  PRIMARY KEY(username)
+);
+
+INSERT INTO users (username, pwd, authkey) VALUES ('yash', 'password', 'testauthkey');
+INSERT INTO users (username, pwd, authkey) VALUES ('rufus', 'password', 'rufusauthkey');
+
+
+drop table if exists replies;
+create table if not exists replies(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  message_id int NOT NULL,
+  channel_id int NOT NULL,
+  user varchar(255) NOT NULL,
+  content text
+);
+
+INSERT INTO replies(message_id, channel_id, user, content) VALUES(1, 0, 'nala', 'this is a reply to yashs first message');
+INSERT INTO replies(message_id, channel_id, user, content) VALUES(2, 0, 'nala', 'this is a reply to another message');
+INSERT INTO replies(message_id, channel_id, user, content) VALUES(1, 0, 'nala', 'this is another reply to yashs first message');
