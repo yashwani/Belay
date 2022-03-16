051dec9a8045405b82ad0bf565dff86b11b802f3
Yash Wani <yashwani@uchicago.edu>
2022-03-16 10:14:01 -0700

    can see replies

--- app.py
+++ app.py
@@ -28,26 +28,46 @@ def channels():
 
 @app.route('/api/messages', methods=['GET'])
 def messages():
-    connection = sqlite3.connect("db/belay.db")
-    cursor = connection.cursor()
-
     authkey = request.headers.get('Authorization')
     channel = request.headers.get('Channel')
 
+    connection = sqlite3.connect("db/belay.db")
+    cursor = connection.cursor()
+
     permission = cursor.execute(
-        "SELECT channel_id from auth where authkey = ? AND channel_id = ?", (authkey, channel)).fetchall()
+        "SELECT * FROM users WHERE authkey = ?", (authkey,)).fetchall()
 
     if len(permission) == 0:
         return jsonify([]), 403
 
     msg = cursor.execute(
-        "select * from messages where channel_id = ? ORDER BY rowid ASC", (channel)).fetchall()
+        "select * from messages where channel_id = ? ORDER BY rowid ASC", (channel,)).fetchall()
 
     messages = []
     for m in msg:
         messages.append([m[2], m[3]])
 
     return jsonify(messages), 200
+
+
+@app.route('/api/replies', methods=['GET'])
+def replies():
+    authkey = request.headers.get('Authorization')
+    channel = request.headers.get('Channel')
+
+    connection = sqlite3.connect("db/belay.db")
+    cursor = connection.cursor()
+
+    permission = cursor.execute(
+        "SELECT * from users where authkey = ? ", (authkey,)).fetchall()
+
+    if len(permission) == 0:
+        return jsonify([]), 403
+
+    rpl = cursor.execute("SELECT message_id, replies.user,replies.content,messages.content from replies inner join messages on message_id = messages.rowid where replies.channel_id = ? order by message_id asc, replies.rowid asc", (channel,)).fetchall()
+    print(rpl)
+
+    return jsonify(rpl), 200
 
 
 @app.route('/api/postMessage', methods=['POST'])
@@ -63,6 +83,40 @@ def postMessage():
         "select username from users where authkey = ?", (authkey,)).fetchall()[0][0]
     cursor.execute(
         "INSERT INTO messages (channel_id, user, content) VALUES (?,?,?)", (channel, user, content,))
+    connection.commit()
+
+    return "", 204
+
+
+@app.route('/api/attemptLogin', methods=['GET'])
+def attemptLogin():
+    username = request.headers.get('Username')
+    password = request.headers.get('Password')
+
+    connection = sqlite3.connect("db/belay.db")
+    cursor = connection.cursor()
+
+    permission = cursor.execute(
+        "SELECT authkey FROM users WHERE username=? AND pwd = ?", (username, password)).fetchall()
+
+    if len(permission) == 0:
+        return "", 403
+
+    return jsonify(permission[0][0]), 200
+
+
+@app.route('/api/createAccount', methods=['POST'])
+def createAccount():
+    body = request.json
+    username = body['username']
+    password = body['password']
+
+    auth_key = str(uuid.uuid4())
+
+    connection = sqlite3.connect("db/belay.db")
+    cursor = connection.cursor()
+    cursor.execute(
+        "INSERT INTO users (username, pwd, authkey) VALUES (?,?,?)", (username, password, auth_key,))
     connection.commit()
 
     return "", 204
--- static/script.js
+++ static/script.js
@@ -1,3 +1,18 @@
+class SendReply extends React.Component {
+
+    render() {
+        return(
+            <div className="sendMessage border">
+                <form>
+                <label >What do you have to say?</label>
+                <textarea name="comment"></textarea>
+                <button onClick={this.props.postReply} type="button" value="Post">Post</button>
+                </form>
+            </div>
+        );
+    }
+}
+
 class SendMessage extends React.Component {
 
     render() {
@@ -14,15 +29,72 @@ class SendMessage extends React.Component {
     }
 }
 
+
+class Replies extends React.Component {
+    render(){
+        const replies = this.props.currentMessageReplies.map((reply, idx) => 
+            <div key ={idx} >
+                <p className = "author" > <b>{reply[1]}</b> </p>
+                <p className = "message" > {reply[2]} </p>
+            </div>
+        )
+
+        if(this.props.currentMessageReplies.length != 0){
+            const original_author = this.props.messages[this.props.currentMessageReplies[0][0]-1][0]
+            const original_message = this.props.messages[this.props.currentMessageReplies[0][0]-1][1]
+            return(
+                <div className="messages border">
+                    <p><b>{original_author}</b></p>
+                    <p>{original_message}</p>
+                    <hr/>
+                    {replies}
+                </div>
+            );
+
+        } else{
+            return (
+                <div className="messages border">
+                    {replies}
+                </div>
+
+            )
+        }
+    }
+}
+
 class Messages extends React.Component {
 
+    componentDidMount(){
+        this.props.fetchMessages();
+        this.props.fetchReplies();
+        
+    }
+
+    countRepliesPerMessage(replies){
+        const mr = {}
+        for (const a of replies) {mr[a[0]-1] = mr[a[0]-1]==null ? 1 : mr[a[0]-1] + 1}
+        return mr
+    }
 
     render() {
+        const message_replies = this.countRepliesPerMessage(this.props.replies)
         const messages = this.props.messages.map((message, idx) => 
-            <div key ={idx} >
-                <p className = "author" > <b>{message[0]}</b> </p>
-                <p className = "message" > {message[1]} </p>
-            </div>
+            {if (message_replies[idx] == null){
+                return (
+                    <div key ={idx} >
+                        <p className = "author" > <b>{message[0]}</b> </p>
+                        <p className = "message" > {message[1]} </p>
+                    </div>
+                )
+            } else{
+                return (
+                    <div key ={idx} >
+                        <p className = "author" > <b>{message[0]}</b> </p>
+                        <p className = "message" > {message[1]} </p>
+                        <button value={idx} onClick={e => this.props.setMessageThread(e.target.value)}> {message_replies[idx] + " Replies"}</button>
+                    </div>
+                )
+            }}
         );
 
         return(
@@ -30,24 +102,37 @@ class Messages extends React.Component {
                 {messages}
             </div>
         );
-
     }
-    
 }
 
 class Chat extends React.Component{
 
     render(){
-        return(
-            <div className = "chat border">
-                <Messages 
-                    messages={this.props.messages}
-                    fetchMessages={this.props.fetchMessages}
-                    currentChannel={this.props.currentChannel}
-                />
-                <SendMessage postMessage={this.props.postMessage}/>
-            </div>
-        )
+        {if (this.props.replyMode){
+            return (
+                <div className = "chat border">
+                    <Replies 
+                        currentMessageReplies = {this.props.currentMessageReplies}
+                        messages = {this.props.messages}
+                    />
+                    <SendMessage postMessage={this.props.postMessage}/>
+                </div>
+            )
+        } else
+            return(
+                <div className = "chat border">
+                    <Messages 
+                        messages={this.props.messages}
+                        fetchMessages={this.props.fetchMessages}
+                        fetchReplies={this.props.fetchReplies}
+                        currentChannel={this.props.currentChannel}
+                        replies={this.props.replies}
+                        setMessageThread = {this.props.setMessageThread}
+                    />
+                    <SendMessage postMessage={this.props.postMessage}/>
+                </div>
+                )
+        }
 
     }
 }
@@ -82,6 +167,46 @@ class Channels extends React.Component{
     }
 }
 
+
+class LoginPage extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <div className="createAccount">
+                <h2>Create an account!</h2>
+                <p>Enter a username</p>
+                <input id="createAccountUsername"></input>
+
+                <p>Enter a password</p>
+                <input id="createAccountPassword"></input>
+
+                <button onClick={this.props.createAccount}>Create Account</button>
+                {/* <p id="acctCreated"> Account created</p> */}
+
+                </div>
+
+                
+                <div className="login">
+                <h2>Log in to Watch Party!</h2>
+                <p>Enter your username</p>
+                <input id="loginUsername"></input>
+
+                <p>Enter your password</p>
+                <input id="loginPassword"></input>
+                <button onClick={this.props.attemptLogin}>Log in</button>
+                {this.props.isInvalidCredential 
+                    ? <p> Invalid Credentials. </p>
+                    : <p></p>
+                }
+                </div>
+            </div>
+        )
+    }
+}
+
+
+
 class Belay extends React.Component{
     constructor(props) {
         super(props);
@@ -89,17 +214,36 @@ class Belay extends React.Component{
         this.switchChannel = this.switchChannel.bind(this);
         this.fetchMessages = this.fetchMessages.bind(this);
         this.postMessage = this.postMessage.bind(this);
+        this.attemptLogin = this.attemptLogin.bind(this);
+        this.createAccount = this.createAccount.bind(this);
+        this.fetchReplies = this.fetchReplies.bind(this);
+        this.setMessageThread = this.setMessageThread.bind(this);
 
         this.state = {
             channelNames: [],
             channelIds: [],
             currentChannel: 0,
-            messages: [] //messages to be displayed on a certain page
+            messages: [], //messages to be displayed on a certain page
+            replies: [],
+
+            isLoggedOut: false,
+            isInvalidCredential: false,
+
+            replyMode: false,
+            currentMessage: 0,
+            currentMessageReplies: []
+
         };
 
-        this.fetchChannels();
-        this.fetchMessages();
+    
 
+    }
+
+    componentDidMount(){
+        this.fetchChannels();
+        
+
+        this.isInvalid = false
     }
 
 
@@ -135,7 +279,7 @@ class Belay extends React.Component{
                 headers: {
                     'Content-Type': 'application/json',
                     'Accept': 'application/json',
-                    'Authorization': 'testauthkey',
+                    'Authorization': window.localStorage.yashwani_auth_key,
                     'Channel': this.state.currentChannel
                 }
             }
@@ -143,12 +287,40 @@ class Belay extends React.Component{
         .then(data => this.setState({messages: data}))
         .then(setTimeout(() => {this.fetchMessages()}, 1000))
     }
-    
+
+    fetchReplies(){
+        console.log('fetching replies')
+        fetch("http://127.0.0.1:5002/api/replies",
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': window.localStorage.yashwani_auth_key,
+                    'Channel': this.state.currentChannel
+                }
+            }
+        ).then(response => response.json())
+        .then(data => this.setState({replies: data}))
+        .then(() => {
+            const currentMessageReplies = []
+            this.state.replies.map((reply, idx) =>        
+            {
+                if(this.state.messageThread == reply[0]){
+                    currentMessageReplies.push(reply)      
+                } 
+            }
+            )
+            this.setState({currentMessageReplies: currentMessageReplies})
+        })
+        .then(setTimeout(() => {this.fetchReplies()}, 1000))
+    }
+
 
     switchChannel(channel){
         this.setState({currentChannel: channel}, 
-            () => {this.fetchMessages();} //because setting state is async
-        )
+            () => { history.pushState("","", window.location.origin + "/chat" + '/' +  this.state.currentChannel); } //because setting state is async
+        )       
     }
 
     postMessage(){
@@ -161,7 +333,7 @@ class Belay extends React.Component{
                 },
                 body: JSON.stringify({
                     'channel': this.state.currentChannel,
-                    'authkey': 'testauthkey',
+                    'authkey': window.localStorage.yashwani_auth_key,
                     'content': document.querySelector("textarea").value
                 })
             }
@@ -169,29 +341,126 @@ class Belay extends React.Component{
         document.querySelector("textarea").value = ''
     }
 
+    attemptLogin(){
+        const username = document.querySelector('#loginUsername').value;
+        const password = document.querySelector('#loginPassword').value;
+        document.querySelector('#loginUsername').value = ''
+        document.querySelector('#loginPassword').value = ''
+        
 
-    
+        fetch("http://127.0.0.1:5002/api/attemptLogin",
+            {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Username': username,
+                    'Password': password
+                }
+            }
+        ).then(response => {
+                if (response.status == 403){
+                    this.setState({isInvalidCredential: true})
+                } else{
+                    response.json()
+                    .then(data => {
+                        window.localStorage.setItem('yashwani_auth_key',data);
+                        this.setState({isLoggedOut: false});
+                    })
+                }}
+        )
+    }
+
+    createAccount(){
+        console.log('creating new account')
+        const username = document.querySelector('#createAccountUsername').value;
+        const password = document.querySelector('#createAccountPassword').value;
+
+        fetch("http://127.0.0.1:5002/api/createAccount",
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                },
+                body: JSON.stringify({
+                    'username': username,
+                    'password': password
+                })
+            }
+        )
+
+    }
+
+
+    setMessageThread(message_idx){
+        // this.setState({messageThread: parseInt(message_idx)+1})
+        // this.setState({replyMode: true})
+
+        this.setState({messageThread: parseInt(message_idx)+1}, 
+            () => { this.setState({replyMode: true}); } //because setting state is async
+        ) 
+
+        // console.log(this.state.messages)
+        // console.log(this.state.messageThread)
+    }
+
+    // logReplies(message_idx){
+    //     this.setState({replyMode: true})
+
+    //     message_idx = parseInt(message_idx)
+
+    //     const currentMessageReplies = []
+    //     this.state.replies.map((reply, idx) =>        
+    //         {
+    //             if(message_idx+1 == reply[0]){
+    //                 currentMessageReplies.push(reply)      
+    //             }      
+    //         }
+    //     )
+
+    //     console.log(replies)
+    //     this.setState({replies: replies})
+    // }
+
 
 
 
     render(){
-        return(
-            <div className = "outerContainer">
-                <Channels 
-                    channelNames={this.state.channelNames} 
-                    channelIds = {this.state.channelIds} 
-                    fetchChannels={this.fetchChannels}
-                    currentChannel={this.state.currentChannel}
-                    switchChannel={this.switchChannel}
+
+        if (this.state.isLoggedOut){
+            return (
+                <LoginPage 
+                    attemptLogin={this.attemptLogin}
+                    isInvalidCredential={this.state.isInvalidCredential}
+                    createAccount={this.createAccount}
                 />
-                <Chat 
-                    messages={this.state.messages}
-                    fetchMessages={this.fetchMessages}
-                    currentChannel={this.state.currentChannel}
-                    postMessage = {this.postMessage}
-                />
-            </div>
-        )
+            )
+        } else{
+            history.pushState("","", window.location.origin + "/chat" + '/' +  this.state.currentChannel); 
+            return(
+                <div className = "outerContainer">
+                    <Channels 
+                        channelNames={this.state.channelNames} 
+                        channelIds = {this.state.channelIds} 
+                        fetchChannels={this.fetchChannels}
+                        currentChannel={this.state.currentChannel}
+                        switchChannel={this.switchChannel}
+                    />
+                    <Chat 
+                        messages={this.state.messages}
+                        fetchMessages={this.fetchMessages}
+                        replies={this.state.replies}
+                        fetchReplies={this.fetchReplies}
+                        currentChannel={this.state.currentChannel}
+                        postMessage = {this.postMessage}
+                        setMessageThread = {this.setMessageThread}
+                        replyMode = {this.state.replyMode}
+                        currentMessageReplies = {this.state.currentMessageReplies}
+                    />
+                </div>
+            )
+        }
     }
 }
 
